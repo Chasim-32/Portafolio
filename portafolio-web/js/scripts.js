702d04c4dc821aa32e402dcc5493f3f665e67c7f
Chasim-32 <jose.manutorres19@gmail.com>
2026-08-30 10:39:21 -0500

    Primer commit - portafolio-web

---
+++ portafolio-web/js/scripts.js
@@ -0,0 +1,39 @@
+
+
+
+// ==============================
+// MODO CLARO / OSCURO
+// ==============================
+
+const themeButton = document.getElementById("themeButton");
+
+// Revisar si ya existe un tema guardado
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    themeButton.textContent = "☀️";
+}
+
+// Cambiar tema
+themeButton.addEventListener("click", function () {
+
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        themeButton.textContent = "☀️";
+    } else {
+        localStorage.setItem("theme", "light");
+        themeButton.textContent = "🌙";
+    }
+
+});
+
+
+// ==============================
+// SESSION STORAGE
+// ==============================
+
+sessionStorage.setItem("visit", "true");
+
